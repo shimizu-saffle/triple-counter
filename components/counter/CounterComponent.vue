ae9120bb86311a8b18b10af13b1fb3a657a6b8b6
shimizu-saffle <shimizu.saffle@gmail.com>
2023-02-10 16:32:00 +0900

    feat: third counter

--- components/counter/CounterComponent.vue
+++ components/counter/CounterComponent.vue
@@ -1,20 +1,37 @@
 <script lang="ts" setup>
+// 親の状態を使う
+const props = defineProps<{
+  counterId: number;
+  propCount: number;
+  propDoubledCount: number;
+}>();
+const emits = defineEmits(['emitIncrement']);
+
+// ローカルカウンター
 const localCounter = ref<number>(0);
 const doubledLocalCounter = computed<number>(() => localCounter.value * 2);
 
+// グローバルカウンター
 const globalCounter = useGlobalCounterComposable();
 </script>
 
 <template>
   <fieldset>
-    <legend>Counter</legend>
+    <legend>Counter {{ counterId }}</legend>
 
+    <!-- コンポーネント変数を使うカウンター -->
     <button @click.prevent="localCounter++" class="me-1">
       Local: {{ localCounter }} * 2 = {{ doubledLocalCounter }}
     </button>
 
+    <!-- コンポジション内の状態を使うカウンター -->
     <button @click.prevent="globalCounter.increment" class="me-1">
       Global: {{ globalCounter.count }} * 2 = {{ globalCounter.doubledCount }}
+    </button>
+
+    <!-- 親の状態を使うカウンター -->
+    <button @click.prevent="emits('emitIncrement')">
+      Parent: {{ props.propCount }} * 2 = {{ props.propDoubledCount }}
     </button>
   </fieldset>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,16 @@
+<script lang="ts" setup>
+const parentCount = ref<number>(0);
+const parentDoubledCount = computed<number>(() => parentCount.value * 2);
+</script>
+
 <template>
   <div>
     <h1>Triple Counter</h1>
-    <CounterComponent />
+    <CounterComponent
+      counter-id="1"
+      :prop-count="parentCount"
+      :prop-doubled-count="parentDoubledCount"
+      @emit-increment="parentCount++"
+    />
   </div>
 </template>
