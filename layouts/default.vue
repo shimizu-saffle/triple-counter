a2f8799087aba018ec908e93a371c18382f5f3c3
shimizu-saffle <shimizu.saffle@gmail.com>
2023-02-10 16:12:21 +0900

    feat: ヘッダー表示

---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <header>Nuxt3 Tutorial</header>
+    <slot />
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Triple Counter</h1>
+  </div>
+</template>
