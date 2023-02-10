5174dad2db40f885378c3a73863122fa06da874d
shimizu-saffle <shimizu.saffle@gmail.com>
2023-02-10 16:13:28 +0900

    define: CounterComponent

---
+++ components/counter/CounterComponent.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+const localCounter = ref<number>(0);
+const doubledLocalCounter = computed<number>(() => localCounter.value * 2);
+</script>
+
+<template>
+  <fieldset>
+    <legend>Counter</legend>
+    <button @click.prevent="localCounter++">
+      Local: {{ localCounter }} * 2 = {{ doubledLocalCounter }}
+    </button>
+  </fieldset>
+</template>
