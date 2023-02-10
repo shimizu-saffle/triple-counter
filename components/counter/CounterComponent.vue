56cc78be9b45cb929697b9c0d36eb4c498620c28
shimizu-saffle <shimizu.saffle@gmail.com>
2023-02-10 16:23:49 +0900

    feat: 2nd counter

--- components/counter/CounterComponent.vue
+++ components/counter/CounterComponent.vue
@@ -1,13 +1,26 @@
 <script lang="ts" setup>
 const localCounter = ref<number>(0);
 const doubledLocalCounter = computed<number>(() => localCounter.value * 2);
+
+const globalCounter = useGlobalCounterComposable();
 </script>
 
 <template>
   <fieldset>
     <legend>Counter</legend>
-    <button @click.prevent="localCounter++">
+
+    <button @click.prevent="localCounter++" class="me-1">
       Local: {{ localCounter }} * 2 = {{ doubledLocalCounter }}
+    </button>
+
+    <button @click.prevent="globalCounter.increment" class="me-1">
+      Global: {{ globalCounter.count }} * 2 = {{ globalCounter.doubledCount }}
     </button>
   </fieldset>
 </template>
+
+<style scoped>
+button.me-1 {
+  margin-right: 1rem;
+}
+</style>
